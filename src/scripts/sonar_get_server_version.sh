bc7f7a2afbeaa5b8734cfcac6011dd01c3281d58
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2020-11-26 11:14:39 +0100

    minor fix

--- src/scripts/sonar_get_server_version.sh
+++ src/scripts/sonar_get_server_version.sh
@@ -3,5 +3,6 @@ SONAR_API_ENDOINT="${SONAR_HOST_URL}/api/server/version"
 SONAR_SERVER_VERSION=$(curl "$SONAR_API_ENDOINT")
 echo "Sonar Server Version: ${SONAR_SERVER_VERSION}"
 echo "export SONAR_SERVER_VERSION='$SONAR_SERVER_VERSION'" > sonar_version.txt
+cat sonar_version.txt
 echo "export SONAR_SERVER_VERSION='$SONAR_SERVER_VERSION'" >> $BASH_ENV
 source $BASH_ENV
