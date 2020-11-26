216672485ca4e2b577cb4363fccddad79fe46701
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2020-11-26 10:16:22 +0100

    add script to get sonarqube server version

---
+++ src/scripts/sonar_get_server_version.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SONAR_API_ENDOINT="${SONAR_HOST_URL}/api/server/version"
+SONAR_SERVER_VERSION=$(curl "$SONAR_API_ENDOINT")
+echo "Sonar Server Version: ${SONAR_SERVER_VERSION}"
+echo "export SONAR_SERVER_VERSION='$SONAR_SERVER_VERSION'" >> $BASH_ENV
+source $BASH_ENV
