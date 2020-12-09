a5284ca187a428fdef4aea87f245fed73b2360ae
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2020-12-09 10:35:07 +0100

    prepare for new git_checkout command

--- src/scripts/java_install_library.sh
+++ src/scripts/java_install_library.sh
@@ -1,17 +1,11 @@
 #!/bin/bash -ex
-# required for automerge
-git config --global user.email "circleci@cloudesire.com"
-git config --global user.name "CircleCI"
-
 if [ "master" != "$CIRCLE_BRANCH" ]; then
   
   basename=$(basename $REPO_URL)
   REPO_NAME=${basename%.*}
 
   cd ${REPO_DIR}/${REPO_NAME}
-  set +e
-  git checkout "$CIRCLE_BRANCH" && git merge origin/master
-  set -e
   git branch
-  ./mvnw clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
+  ${MAVEN_PATH} ${MAVEN_COMMAND} ${MAVEN_OPTS}
+
 fi;
