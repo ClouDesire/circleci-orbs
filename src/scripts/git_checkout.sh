2861d70e13cd7c18b18b67d2c6ae418879910a88
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2020-12-09 10:37:55 +0100

    add more parameters to git_checkout
    Add the GIT_EMAIL and GIT_USERNAME parameters to set git global config for username and email. 
    Also adds the MERGE_MASTER parameter (true by default). When this is true, the master branch is merged with the current CIRCLE_BRANCH.

--- src/scripts/git_checkout.sh
+++ src/scripts/git_checkout.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 CheckoutRepo() {
-  echo "Cloning repo $REPO_URL on branch $REPO_BRANCH"
+  git config --global user.email "${GIT_EMAIL}"
+  git config --global user.name "${GIT_USERNAME}"
+
   basename=$(basename $REPO_URL)
   REPO_NAME=${basename%.*}
+  
+  echo "Cloning repo $REPO_URL on branch $REPO_BRANCH"
   git clone $REPO_URL --branch $REPO_BRANCH --single-branch "${REPO_DIR}/${REPO_NAME}"
+
+  if [ $MERGE_MASTER -eq 1 ]; then
+    set +e
+    git checkout "$CIRCLE_BRANCH" && git merge origin/master
+    set -e
+  fi
 }
 
 # Will not run if sourced for bats-core tests.
