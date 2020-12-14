39cc99e8f869312e6c7b1217ad06c9f900c71210
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2020-12-14 16:00:58 +0100

    fix maven_install_library command

--- src/scripts/maven_install_library.sh
+++ src/scripts/maven_install_library.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 basename=$(basename $REPO_URL)
 REPO_NAME=${basename%.*}
-cd ${REPO_DIR}/${REPO_NAME}
+
+echo "Installing library '${REPO_NAME}' from folder ${REPO_DIR}/${REPO_NAME}"
+cd "${REPO_DIR}/${REPO_NAME}"
 ${MAVEN_PATH} ${MAVEN_COMMAND} ${MAVEN_OPTS}
 
