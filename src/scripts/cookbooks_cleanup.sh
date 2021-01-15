1d43c9b96a98e35209c798e26f0ffb0b322117b7
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-01-15 15:12:16 +0100

    minor fixes

--- src/scripts/cookbooks_cleanup.sh
+++ src/scripts/cookbooks_cleanup.sh
@@ -3,9 +3,9 @@ export PARAM_KITCHEN_SUITES=$1
 export PARAM_KITCHEN_CONCURRENCY=$2
 
 if [ "$PARAM_KITCHEN_CONCURRENCY" == "true" ]; then
-    export KITCHEN_CONCURRENCY="-c"
+  export KITCHEN_CONCURRENCY="-c"
 else
-        export KITCHEN_CONCURRENCY=""
+  export KITCHEN_CONCURRENCY=""
 fi
 
 retry -- chef exec kitchen destroy "($PARAM_KITCHEN_SUITES)" $KITCHEN_CONCURRENCY
