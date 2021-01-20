62018ef0477eae4940b6fdce3abaef08f3b4b89b
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-01-20 17:56:40 +0100

    fix issue with PR url

--- src/scripts/github_pr_add_comment.sh
+++ src/scripts/github_pr_add_comment.sh
@@ -2,7 +2,7 @@
 
 pr_comment_url=$(curl -s --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/${CIRCLE_PULL_REQUEST##*/}" --header "Authorization: token ${GITHUB_TOKEN}" | jq -r "._links.comments.href")
 
-echo ">> PR Link: ${pr_comment_url}"
+echo ">> PR url: ${pr_comment_url}"
 
 curl --location --request POST "$pr_comment_url" \
 --header 'Authorization: token ${GITHUB_TOKEN}' \
