a988957b8da2271884837e035bde30527285debb
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-01-20 17:41:40 +0100

    fix add_comment script

--- src/scripts/github_pr_add_comment.sh
+++ src/scripts/github_pr_add_comment.sh
@@ -3,6 +3,8 @@
 pr_comment_url=$(curl -s --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/${CIRCLE_PULL_REQUEST##*/}" \
 --header 'Authorization: Bearer ${GITHUB_TOKEN}')
 
+echo ">> PR Link: ${pr_comment_url}"
+
 curl --location --request POST "$pr_comment_url" \
 --header 'Authorization: Bearer ${GITHUB_TOKEN}' \
 --data-raw "{\"body\": \"$(eval echo ${PR_COMMENT})\"}"
