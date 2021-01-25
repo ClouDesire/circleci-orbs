bea824923999031c57fa747045f269e01ff39c45
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-01-25 14:48:45 +0100

    [semver:minor] Add commands and scripts to interact with github PRs (#55)

---
+++ src/scripts/github_pr_read_labels.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ReadPRLabels() {
+
+  echo ">> Org: ${CIRCLE_PROJECT_USERNAME}"
+  echo ">> Repo: ${CIRCLE_PROJECT_REPONAME}"
+  echo ">> PR Link: ${CIRCLE_PULL_REQUEST}"
+  echo ">> PR #: ${CIRCLE_PULL_REQUEST##*/}"
+  labels=$(curl -s --location --request GET "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${CIRCLE_PULL_REQUEST##*/}" --header "Authorization: token ${GITHUB_TOKEN}" | jq -r 'select(.labels != null) | .labels | map(.name) | join(",")')
+
+  echo ">> Labels: ${labels}"
+  IFS=',' read -a labels_array <<< "$labels"
+  for label in "${labels_array[@]}"; do
+    label=${label//-/_}
+    echo "  >> Adding GH_PR_LABEL_${label^^} to BASH_ENV"
+    echo "export GH_PR_LABEL_${label^^}=true" >> "$BASH_ENV"
+  done
+  
+  source "$BASH_ENV"
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    ReadPRLabels
+fi
+
---
+++ src/tests/github_pr_read_labels.bats
@@ -0,0 +1,24 @@
+#!/usr/bin/env bats
+# Runs prior to every test
+setup() {
+  # Load our script file.
+  source ./src/scripts/github_pr_read_labels.sh
+}
+
+#function teardown() {
+#  if [ -f "${BASH_ENV}" ]; then
+#    rm -f "${BASH_ENV}"
+#  fi
+#}
+
+
+@test 'ReadPRLabels exports labels correctly' {
+  export CIRCLE_PROJECT_USERNAME="ClouDesire"
+  export CIRCLE_PROJECT_REPONAME="circleci-orbs"
+  export CIRCLE_PULL_REQUEST="https://github.com/ClouDesire/circleci-orbs/pull/55"
+  export BASH_ENV="/tmp/.pipeline_env"
+  ReadPRLabels
+  echo "$GH_PR_LABEL_TEST_LABEL"
+  [[ $GH_PR_LABEL_TEST_LABEL ]]
+}
+
