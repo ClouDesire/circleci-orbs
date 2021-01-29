c471b8a52850dfdc00ad58f5471b244fd0fd545c
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-01-29 17:23:40 +0100

    [semver:minor] Add repeatable kitchen suites (#60)
    * add repeatable kitchen suites

--- src/scripts/cookbooks_test.sh
+++ src/scripts/cookbooks_test.sh
@@ -21,17 +21,28 @@ chef gem install kitchen-transport-speedy
 LOCAL_SCRIPT='test-local.sh'
 # If local override is present, execute it
 if [ -x $LOCAL_SCRIPT ]; then
-    echo "Executing kitchen from test-local.sh"
-    ./$LOCAL_SCRIPT $PARAM_KITCHEN_SUITES $PARAM_KITCHEN_CONCURRENCY
+  echo "Executing kitchen from test-local.sh"
+  ./$LOCAL_SCRIPT $PARAM_KITCHEN_SUITES $PARAM_KITCHEN_CONCURRENCY
 else
 
-    echo ">> Kitchen Concurrency => $PARAM_KITCHEN_CONCURRENCY"
-    echo ">> Kitchen Suites => $PARAM_KITCHEN_SUITES"
-    export KERNEL_UPGRADE_ACTION="reboot"
-    echo ">> KERNEL_UPGRADE_ACTION => ${KERNEL_UPGRADE_ACTION}"
-    chef exec kitchen converge "($PARAM_KITCHEN_SUITES)" $KITCHEN_CONCURRENCY
-    # https://github.com/inspec/kitchen-inspec/issues/167
-    chef exec kitchen verify "($PARAM_KITCHEN_SUITES)"
+  echo ">> Kitchen Concurrency => $PARAM_KITCHEN_CONCURRENCY"
+  echo ">> Kitchen Suites => $PARAM_KITCHEN_SUITES"
+  echo ">> Kitchen Suites to repeat: $PARAM_KITCHEN_REPEAT_SUITES"
+  export KERNEL_UPGRADE_ACTION="reboot"
+  echo ">> KERNEL_UPGRADE_ACTION => ${KERNEL_UPGRADE_ACTION}"
+  chef exec kitchen converge "($PARAM_KITCHEN_SUITES)" $KITCHEN_CONCURRENCY
+
+  if [ ! -z $PARAM_KITCHEN_REPEAT_SUITES ]; then
+    PARAM_KITCHEN_REPEAT_SUITES="$(echo -e "${PARAM_KITCHEN_REPEAT_SUITES}" | tr -d '[:space:]')"
+
+    echo "Repeat converge for ${PARAM_KITCHEN_REPEAT_SUITES}"
+
+    PARAM_KITCHEN_REPEAT_SUITES=${PARAM_KITCHEN_REPEAT_SUITES//,/|}
+    chef exec kitchen converge "($PARAM_KITCHEN_REPEAT_SUITES)"
+  fi
+
+  # https://github.com/inspec/kitchen-inspec/issues/167
+  chef exec kitchen verify "($PARAM_KITCHEN_SUITES)"
 fi
 
 exit 0
