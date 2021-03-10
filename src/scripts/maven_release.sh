11b4cf87c04b9318c2738f21bdf3d11636d98836
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-10 15:54:45 +0100

    fix issue

--- src/scripts/maven_release.sh
+++ src/scripts/maven_release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MavenRelease() {
-  VERSION=$(echo ${CIRCLE_BRANCH//release-})
+  VERSION="${CIRCLE_BRANCH//release-}"
 
-  cd $PROJECT_DIR
+  cd "$PROJECT_DIR"
 
   echo "Releasing $VERSION" 
   #./mvnw deploy -Dmaven.test.skip=true
@@ -14,10 +14,10 @@ MavenRelease() {
   git config --global user.name "${GIT_USERNAME}"
 
 
-  IFS='.' read -a semver <<< "$VERSION"
+  IFS='.' read -ar semver <<< "$VERSION"
   NEW_VERSION="${semver[0]}.${semver[1]}.$((${semver[2]} + 1))"
   echo "Updating pom.xml version to $NEW_VERSION"
-  ./mvnw versions:set -DnewVersion=${NEXT_VERSION}-SNAPSHOT scm:checkin -Dmessage="[skip ci] Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
+  ./mvnw versions:set -DnewVersion="${NEW_VERSION}-SNAPSHOT" scm:checkin -Dmessage="[skip ci] Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
 
 }
 
