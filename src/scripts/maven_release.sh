dde5f0a82cd89a58b4404f937507b244c9268c86
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-10 10:19:33 +0100

    update script maven_release

--- src/scripts/maven_release.sh
+++ src/scripts/maven_release.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 MavenRelease() {
-  NEW_VERSION=$(echo ${CIRCLE_BRANCH//release-})
+  VERSION=$(echo ${CIRCLE_BRANCH//release-})
 
   cd $PROJECT_DIR
 
-  echo "Releasing $NEW_VERSION" 
+  echo "Releasing $VERSION" 
   #./mvnw deploy -Dmaven.test.skip=true
+  #./mvnw scm:tag -Dtag=v${VERSION}
 
-
-  IFS='.' read -a semver <<< "$NEW_VERSION"
+  IFS='.' read -a semver <<< "$VERSION"
   NEW_VERSION="${semver[0]}.${semver[1]}.$((${semver[2]} + 1))"
   echo "Updating pom.xml version to $NEW_VERSION"
-  ./mvnw versions:set -DnewVersion=${NEW_VERSION}-SNAPSHOT -DgenerateBackupPoms=false
+  ./mvnw versions:set -DnewVersion=${NEXT_VERSION}-SNAPSHOT scm:checkin -Dmessage="[skip ci] Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
 
 }
 
