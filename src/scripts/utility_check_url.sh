84a38aca78f223c182c12f175f20221da0647cb6
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-19 12:44:59 +0100

    minor changes

--- src/scripts/utility_check_url.sh
+++ src/scripts/utility_check_url.sh
@@ -4,7 +4,10 @@ if [ "${CONTAINER_NAME}" == "" ]; then
   CONTAINER_NAME="$CIRCLE_PROJECT_REPONAME"
 fi
 
-docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 --retry "30" --retry-delay 1 --max-time 10 --retry-connrefused "${CHECK_URL}"
+echo "Container name: ${CONTAINER_NAME}"
+echo "URL: ${CHECK_URL}"
+
+docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 --retry "30" --retry-delay 1 --max-time 10 --retry-all-errors "${CHECK_URL}"
 
 #if [ "${EXECUTOR_IS_DOCKER}" == "true" ]; then
 #else
