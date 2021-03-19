e831eee7f2270810cb5fd808b9f37ee9edee4469
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-19 14:27:17 +0100

    minor changes

--- src/scripts/utility_check_url.sh
+++ src/scripts/utility_check_url.sh
@@ -5,16 +5,9 @@ if [ "${CONTAINER_NAME}" == "" ]; then
 fi
 
 echo "Container name: ${CONTAINER_NAME}"
+echo "Retries: ${CHECK_RETRIES}"
+echo "Sleep time: ${SLEEP_TIME}"
 echo "URL: ${CHECK_URL}"
 
-docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 --retry "30" --retry-delay 1 --max-time 10 --retry-all-errors "${CHECK_URL}"
+docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 --retry "${CHECK_RETRIES}" --retry-delay ${SLEEP_TIME} --max-time 10 --retry-all-errors "${CHECK_URL}"
 
-#if [ "${EXECUTOR_IS_DOCKER}" == "true" ]; then
-#else
-#  for i in $(seq 1 ${RETRIES}); do
-#    if [ $i -gt 1 ] && sleep ${SLEEP_TIME}; then
-#      curl -sSf "${CHECK_URL}" && s=0 && break || s=$?;
-#    fi
-#  done;
-#  exit $s
-#fi
