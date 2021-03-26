1a9451e2b62d0ba072667e136461b95bc5063556
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-26 16:55:17 +0100

    add check on the HTTP_CODE returned by the curl

--- src/scripts/utility_check_url.sh
+++ src/scripts/utility_check_url.sh
@@ -9,5 +9,9 @@ echo "Retries: ${CHECK_RETRIES}"
 echo "Sleep time: ${SLEEP_TIME}"
 echo "URL: ${CHECK_URL}"
 
-docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 --retry "${CHECK_RETRIES}" --retry-delay ${SLEEP_TIME} --max-time 10 --retry-all-errors "${CHECK_URL}"
+HTTP_RESPONSE_CODE=$(docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 -s -o /dev/null -w "%{http_code}" --retry "${CHECK_RETRIES}" --retry-delay ${SLEEP_TIME} --max-time 10 --retry-all-errors "${CHECK_URL}")
 
+echo "Request response code: ${HTTP_RESPONSE_CODE}"
+if [ "${HTTP_RESPONSE_CODE}" != "200" ]; then
+  exit 1
+fi
