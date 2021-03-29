e6be5773b9652be7348ce03e62ff4d5c94f2760c
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-29 16:12:51 +0200

    add echo of MAVEN_COMMAND, MAVEN_PATH and MAVEN_OPTS

--- src/scripts/maven_install_library.sh
+++ src/scripts/maven_install_library.sh
@@ -3,6 +3,10 @@ basename=$(basename $REPO_URL)
 REPO_NAME=${basename%.*}
 
 echo "Installing library '${REPO_NAME}' from folder ${REPO_DIR}/${REPO_NAME}"
+echo "Maven path: ${MAVEN_PATH}"
+echo "Maven command: ${MAVEN_COMMAND}"
+echo "Maven options: ${MAVEN_OPTS}
+
 cd "${REPO_DIR}/${REPO_NAME}"
 ${MAVEN_PATH} ${MAVEN_COMMAND} ${MAVEN_OPTS}
 
