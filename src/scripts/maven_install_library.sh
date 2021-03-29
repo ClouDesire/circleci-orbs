d21649dba0c047545d3705291566af2377eef11c
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-29 16:19:42 +0200

    Update maven_install_library.sh

--- src/scripts/maven_install_library.sh
+++ src/scripts/maven_install_library.sh
@@ -5,7 +5,7 @@ REPO_NAME=${basename%.*}
 echo "Installing library '${REPO_NAME}' from folder ${REPO_DIR}/${REPO_NAME}"
 echo "Maven path: ${MAVEN_PATH}"
 echo "Maven command: ${MAVEN_COMMAND}"
-echo "Maven options: ${MAVEN_OPTS}
+echo "Maven options: ${MAVEN_OPTS}"
 
 cd "${REPO_DIR}/${REPO_NAME}"
 ${MAVEN_PATH} ${MAVEN_COMMAND} ${MAVEN_OPTS}
