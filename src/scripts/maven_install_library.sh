ea50a1a28f4fff7aed4b66e55dcae4131a553303
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-29 16:47:31 +0200

    change MAVEN_OPTS to MAVEN_CMD_OPTS
    [skip ci]

--- src/scripts/maven_install_library.sh
+++ src/scripts/maven_install_library.sh
@@ -5,8 +5,8 @@ REPO_NAME=${basename%.*}
 echo "Installing library '${REPO_NAME}' from folder ${REPO_DIR}/${REPO_NAME}"
 echo "Maven path: ${MAVEN_PATH}"
 echo "Maven command: ${MAVEN_COMMAND}"
-echo "Maven options: ${MAVEN_OPTS}"
+echo "Maven options: ${MAVEN_CMD_OPTS}"
 
 cd "${REPO_DIR}/${REPO_NAME}"
-${MAVEN_PATH} ${MAVEN_COMMAND} "${MAVEN_OPTS}"
+${MAVEN_PATH} ${MAVEN_COMMAND} "${MAVEN_CMD_OPTS}"
 
