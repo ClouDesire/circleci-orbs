12abbb68a21a96380b2a2b97a801e59a5750dbbc
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-30 16:44:41 +0200

    add command and script for git_merge_master

--- src/scripts/git_merge_master.sh
+++ src/scripts/git_merge_master.sh
@@ -1,6 +1,40 @@
 #!/bin/bash -e
 # Make sure that current branch is aligned to master
+GitMergeMaster() {
+  git clean -dxf
+  
+  REPO_URL="${CIRCLE_REPOSITORY_URL}"
 
-git clean -dxf
-git config --global user.email "circleci@cloudesire.com" && git config --global user.name "CircleCI"
-if [ "master" != $CIRCLE_BRANCH ]; then git fetch origin master && git merge --no-edit origin/master; fi
+  git config user.email "${GIT_EMAIL}"
+  git config user.name "${GIT_USERNAME}"
+
+  if [[ "$REPO_URL" == git@github.com* ]]; then
+    REPO_URL=${REPO_URL#"git@github.com:"}
+    REPO_URL="https://$GITHUB_TOKEN:x-oauth-basic@github.com/${REPO_URL}"
+  fi
+
+
+  if [ -z $DEFAULT_BRANCH ]; then
+    if git ls-remote -h $REPO_URL | grep -q "refs/heads/master"; then
+      DEFAULT_BRANCH="master"
+    elif git ls-remote -h $REPO_URL | grep -q "refs/heads/main"; then
+      DEFAULT_BRANCH="main"
+    else
+      echo "ERROR: impossible to find a remote branch that it is either master or main"
+      return 1
+    fi
+  fi
+
+  if [ "${DEFAULT_BRANCH}" != "${CIRCLE_BRANCH}" ]; then 
+    git fetch origin "${DEFAULT_BRANCH}" 
+    git merge --no-edit origin/"${DEFAULT_BRANCH}"; 
+  fi
+}
+
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+  GitMergeMaster
+fi
