6080876884a668ddf2e6fb03cd0e5ea3a84d23e9
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-30 17:11:48 +0200

    minor fix

--- src/scripts/git_merge_master.sh
+++ src/scripts/git_merge_master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 # Make sure that current branch is aligned to master
 GitMergeMaster() {
-  git clean -dxf
   
   REPO_URL="${CIRCLE_REPOSITORY_URL}"
+  DEFAULT_BRANCH=""
 
   git config user.email "${GIT_EMAIL}"
   git config user.name "${GIT_USERNAME}"
@@ -13,19 +13,17 @@ GitMergeMaster() {
     REPO_URL="https://$GITHUB_TOKEN:x-oauth-basic@github.com/${REPO_URL}"
   fi
 
-
-  if [ -z $DEFAULT_BRANCH ]; then
-    if git ls-remote -h $REPO_URL | grep -q "refs/heads/master"; then
-      DEFAULT_BRANCH="master"
-    elif git ls-remote -h $REPO_URL | grep -q "refs/heads/main"; then
-      DEFAULT_BRANCH="main"
-    else
-      echo "ERROR: impossible to find a remote branch that it is either master or main"
-      return 1
-    fi
+  if git ls-remote -h $REPO_URL | grep -q "refs/heads/master"; then
+    DEFAULT_BRANCH="master"
+  elif git ls-remote -h $REPO_URL | grep -q "refs/heads/main"; then
+    DEFAULT_BRANCH="main"
+  else
+    echo "ERROR: impossible to find a remote branch that it is either master or main"
+    return 1
   fi
 
-  if [ "${DEFAULT_BRANCH}" != "${CIRCLE_BRANCH}" ]; then 
+  if [ "${DEFAULT_BRANCH}" != "${CIRCLE_BRANCH}" ]; then
+    git clean -dxf
     git fetch origin "${DEFAULT_BRANCH}" 
     git merge --no-edit origin/"${DEFAULT_BRANCH}"; 
   fi
