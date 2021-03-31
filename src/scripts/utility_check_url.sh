407032be8dfd89a5ed4f2159edfa090e47af8e3a
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-03-31 09:37:49 +0200

    add use of curl option -f

--- src/scripts/utility_check_url.sh
+++ src/scripts/utility_check_url.sh
@@ -4,14 +4,24 @@ if [ "${CONTAINER_NAME}" == "" ]; then
   CONTAINER_NAME="$CIRCLE_PROJECT_REPONAME"
 fi
 
+if [ "${CURL_IMG_VERSION}" == "" ]; then
+  CONTAINER_NAME="7.75.0"
+fi
+
 echo "Container name: ${CONTAINER_NAME}"
 echo "Retries: ${CHECK_RETRIES}"
 echo "Sleep time: ${SLEEP_TIME}"
 echo "URL: ${CHECK_URL}"
 
-HTTP_RESPONSE_CODE=$(docker run --network "container:${CONTAINER_NAME}" --rm curlimages/curl:7.75.0 -s -o /dev/null -w "%{http_code}" --retry "${CHECK_RETRIES}" --retry-delay ${SLEEP_TIME} --max-time 10 --retry-all-errors "${CHECK_URL}")
+docker run --network "container:${CONTAINER_NAME}" --rm "curlimages/curl:{CURL_IMG_VERSION}" \
+  -f \
+  --retry ${CHECK_RETRIES} \
+  --retry-delay ${SLEEP_TIME} \
+  --max-time 10 \
+  --retry-all-errors \
+  "${CHECK_URL}"
 
-echo "Request response code: ${HTTP_RESPONSE_CODE}"
-if [ "${HTTP_RESPONSE_CODE}" != "200" ]; then
+if [ $? -ne 0 ]; then
   exit 1
 fi
+
