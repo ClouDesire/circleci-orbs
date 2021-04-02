cac8f729b3950bac914573f0e47730247adc0351
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-02 12:00:42 +0200

    add check on -SNAPSHOT and add git tag

--- src/scripts/maven_release.sh
+++ src/scripts/maven_release.sh
@@ -2,10 +2,15 @@
 
 MavenRelease() {
   
+  RELEASE_VERSION=$(${MVN_PATH} help:evaluate -Dexpression=project.version -q -DforceStdout)
+  if [[ "${RELEASE_VERSION}" == *"-SNAPSHOT"* ]]; then
+    echo "Skipping release because version contains SNAPSHOT. Version: ${RELEASE_VERSION}"
+    circleci-agent step halt
+  fi
+
   git config --global user.email "${GIT_EMAIL}"
   git config --global user.name "${GIT_USERNAME}"
 
-  RELEASE_VERSION=$(${MVN_PATH} help:evaluate -Dexpression=project.version -q -DforceStdout)
   echo "export MAVEN_RELEASE_VERSION=${RELEASE_VERSION}" >> "${BASH_ENV}"
   
   cd "$PROJECT_DIR"
@@ -13,14 +18,19 @@ MavenRelease() {
   echo "Releasing $RELEASE_VERSION"
   ${MVN_PATH} deploy -Dmaven.test.skip=true
   
+  echo "Creating git tag"
+  git tag "${RELEASE_VERSION}"
+
+
   IFS='.' read -a semver <<< "$RELEASE_VERSION"
   NEW_VERSION="${semver[0]}.${semver[1]}.$((${semver[2]} + 1))"
   echo "Updating pom.xml version to $NEW_VERSION"
   ${MVN_PATH} versions:set -DnewVersion="${NEW_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
 
   find ./ -name pom.xml -exec git add {} \;
-  git commit -m "Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT" -m "" -m "[skip ci]"
-  git push --set-upstream origin "${CIRCLE_BRANCH}"
+  git commit -m "Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT"
+  
+  git push --set-upstream origin "${CIRCLE_BRANCH}" --tags
 
 }
 
