4ed28b59bffd0a17711c15615b832633eae3c9ef
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-06 11:32:58 +0200

    [semver:patch] Add tag creation on maven release (#89)

--- src/scripts/maven_release.sh
+++ src/scripts/maven_release.sh
@@ -2,25 +2,24 @@
 
 MavenRelease() {
   
-  git config --global user.email "${GIT_EMAIL}"
-  git config --global user.name "${GIT_USERNAME}"
-
-  RELEASE_VERSION=$(${MVN_PATH} help:evaluate -Dexpression=project.version -q -DforceStdout)
-  echo "export MAVEN_RELEASE_VERSION=${RELEASE_VERSION}" >> "${BASH_ENV}"
-  
   cd "$PROJECT_DIR"
 
   echo "Releasing $RELEASE_VERSION"
   ${MVN_PATH} deploy -Dmaven.test.skip=true
   
+  echo "Creating git tag"
+  git tag "${RELEASE_VERSION}"
+
+
   IFS='.' read -a semver <<< "$RELEASE_VERSION"
   NEW_VERSION="${semver[0]}.${semver[1]}.$((${semver[2]} + 1))"
   echo "Updating pom.xml version to $NEW_VERSION"
   ${MVN_PATH} versions:set -DnewVersion="${NEW_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
 
   find ./ -name pom.xml -exec git add {} \;
-  git commit -m "Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT" -m "" -m "[skip ci]"
-  git push --set-upstream origin "${CIRCLE_BRANCH}"
+  git commit -m "Preparing for next iteration - version set to ${NEW_VERSION}-SNAPSHOT"
+  
+  git push --set-upstream origin "${CIRCLE_BRANCH}" --tags
 
 }
 
