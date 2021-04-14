1bb06e78e487a8777bdfb332c2868caea3a297e9
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-14 12:35:12 +0200

    add support for comment content on file

--- src/scripts/github_pr_add_comment.sh
+++ src/scripts/github_pr_add_comment.sh
@@ -9,10 +9,19 @@ AddPRComment() {
   pr_comment_url=$(curl -s --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/${CIRCLE_PULL_REQUEST##*/}" --header "Authorization: token ${GITHUB_TOKEN}" | jq -r "._links.comments.href")
 
   echo ">> PR url: ${pr_comment_url}"
-  echo ">> Comment: $(eval echo ${PR_COMMENT})"
+
+  if [ -n "$PR_COMMENT_FILE_PATH" ]; then
+    PR_COMMENT=$(eval cat "${PR_COMMENT_FILE_PATH}")
+    echo -e ">> Comment: \n ${PR_COMMENT})"
+  else 
+    PR_COMMENT=$(eval echo "${PR_COMMENT}")
+    echo ">> Comment: $PR_COMMENT"
+  fi
+
+  
   curl --location --request POST "$pr_comment_url" \
   --header "Authorization: token ${GITHUB_TOKEN}" \
-  --data-raw "{\"body\": \"$(eval echo ${PR_COMMENT})\"}"
+  --data-raw "{\"body\": \"$PR_COMMENT\"}"
 }
 
 
