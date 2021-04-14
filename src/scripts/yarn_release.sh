d07dc9419a658c45c09e35551f4f8d19626e3378
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-14 10:57:38 +0200

    [semver:minor] Add support for yarn release (#99)

---
+++ src/scripts/yarn_release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+YarnRelease() {
+  
+  cd "$PROJECT_DIR"
+
+  echo "Setting up .npmrc"
+  NPM_REGISTRY_HOSTNAME="${NPM_REGISTRY#'http://'}"
+  NPM_REGISTRY_HOSTNAME="${NPM_REGISTRY_HOSTNAME#'https://'}"
+  NPM_REGISTRY_HOSTNAME="${NPM_REGISTRY_HOSTNAME%'/'}"
+
+  echo "//$NPM_REGISTRY_HOSTNAME/:_authToken=$NPM_TOKEN" > "${HOME}/.npmrc"
+  
+  echo "Releasing $RELEASE_VERSION"
+  npm publish --registry $NPM_REGISTRY
+
+  echo "Tagging commit"
+  git tag "v${RELEASE_VERSION}"
+
+  echo "Calculating next version"
+  IFS='.' read -a semver <<< "$RELEASE_VERSION"
+  NEW_VERSION="${semver[0]}.${semver[1]}.$((${semver[2]} + 1))"
+  NEW_VERSION="${NEW_VERSION}-beta"
+  
+  yarn config set version-git-tag false
+  yarn config set version-commit-hooks false
+
+  echo "Updating package.json version to ${NEW_VERSION}"
+  yarn version --new-version "${NEW_VERSION}"
+
+  find ./ -name package.json -not -path "./node_modules/*" -exec git add {} \;
+  git commit -m "Preparing for next iteration - version set to ${NEW_VERSION}"
+  
+  git push --set-upstream origin "${CIRCLE_BRANCH}" --tags
+
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+  YarnRelease
+fi
