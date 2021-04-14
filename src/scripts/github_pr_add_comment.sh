342c19664f208ff0355df90d50e19c041ff4b13f
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-14 16:23:18 +0200

    fix pr comment preparation

--- src/scripts/github_pr_add_comment.sh
+++ src/scripts/github_pr_add_comment.sh
@@ -9,13 +9,18 @@ AddPRComment() {
   pr_comment_url=$(curl -s --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/${CIRCLE_PULL_REQUEST##*/}" --header "Authorization: token ${GITHUB_TOKEN}" | jq -r "._links.comments.href")
 
   echo ">> PR url: ${pr_comment_url}"
-
+  
   if [ -n "$PR_COMMENT_FILE_PATH" ]; then
-    sed -i '$! s/$/\\n/' "${PR_COMMENT_FILE_PATH}"
-    tr -d '\n' < "${PR_COMMENT_FILE_PATH}" > tmp.json
-    mv -f tmp.json "${PR_COMMENT_FILE_PATH}"
-    sed -i 's/\"/\\\"/g' "${PR_COMMENT_FILE_PATH}"
-    PR_COMMENT=$(cat "${PR_COMMENT_FILE_PATH}")
+    file=$(eval echo $PR_COMMENT_FILE_PATH)
+    
+    sed -i '$! s/$/\\n/' "${file}"
+    tr -d '\n' < "${file}" > tmp.json
+    
+    mv -f tmp.json "${file}"
+    
+    sed -i 's/\"/\\\"/g' "${file}"
+    
+    PR_COMMENT=$(cat "${file}")
     echo -e ">> Comment: \n ${PR_COMMENT}"
   else 
     PR_COMMENT=$(eval echo "${PR_COMMENT}")
