b9e1df0488a4ae885217b82afff0d163015da924
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-04-19 10:21:15 +0200

    [semver:minor] Add support for PR comment from file (#100)

--- src/scripts/github_pr_add_comment.sh
+++ src/scripts/github_pr_add_comment.sh
@@ -9,10 +9,30 @@ AddPRComment() {
   pr_comment_url=$(curl -s --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/${CIRCLE_PULL_REQUEST##*/}" --header "Authorization: token ${GITHUB_TOKEN}" | jq -r "._links.comments.href")
 
   echo ">> PR url: ${pr_comment_url}"
-  echo ">> Comment: $(eval echo ${PR_COMMENT})"
+  
+  PR_COMMENT=$(eval echo "${PR_COMMENT}")
+
+  if [ -n "$PR_COMMENT_FILE_PATH" ]; then
+    file=$(eval echo $PR_COMMENT_FILE_PATH)
+    
+    sed -i '$! s/$/\\n/' "${file}"
+    tr -d '\n' < "${file}" > tmpfile
+    
+    mv -f tmpfile "${file}"
+    
+    sed -i 's/\"/\\\"/g' "${file}"
+    
+    PR_COMMENT="${PR_COMMENT}\n$(cat ${file})"
+    echo -e ">> Comment: \n ${PR_COMMENT}"
+  fi
+
+  
+  echo -nE "{\"body\":\"${PR_COMMENT}\"}" > request_body.json
+  
   curl --location --request POST "$pr_comment_url" \
   --header "Authorization: token ${GITHUB_TOKEN}" \
-  --data-raw "{\"body\": \"$(eval echo ${PR_COMMENT})\"}"
+  --header 'Content-Type: text/json' \
+  -d @request_body.json
 }
 
 
