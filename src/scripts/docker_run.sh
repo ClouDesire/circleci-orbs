3d1c93750cd4e8bb49283b637b1ac94796fdae40
Giovanni Toraldo <giovanni.toraldo@eng.it>
2021-05-04 10:09:13 +0200

    Expose image name and version

--- src/scripts/docker_run.sh
+++ src/scripts/docker_run.sh
@@ -8,8 +8,13 @@ if [ "${CONTAINER_NAME}" == "" ]; then
   CONTAINER_NAME="$CIRCLE_PROJECT_REPONAME"
 fi
 
+if [ "${IMAGE_NAME}" == "" ]; then
+  IMAGE_NAME="$CONTAINER_NAME"
+fi
+
 echo "Container name: ${CONTAINER_NAME}"
 echo "Container port range: ${CONTAINER_PORT_RANGE}"
 echo "Docker options: ${DOCKER_CMD_OPTIONS}"
+echo "Image ref: ${IMAGE_NAME}:${IMAGE_VERSION}"
 
-docker run -d --name "${CONTAINER_NAME}" -p "${CONTAINER_PORT_RANGE}" ${DOCKER_CMD_OPTIONS} "${DOCKER_REGISTRY}/${CONTAINER_NAME}:${CONTAINER_VERSION}"
+docker run -d --name "${CONTAINER_NAME}" -p "${CONTAINER_PORT_RANGE}" ${DOCKER_CMD_OPTIONS} "${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}"
