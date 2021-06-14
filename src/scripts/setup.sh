f02dbe86dd4d19eb497d60e34b31d0597216c118
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-06-14 12:42:08 +0200

    apply review suggestions

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -8,19 +8,19 @@ function install_jq() {
   jq_url="https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64"
   jq_installation_path='/usr/local/bin/jq'
 
-  if ! command -v 'jq' &> /dev/null; then
-    if ! command -v 'wget' &> /dev/null; then
-      echo "ERROR: impossible to find command wget. Trying with curl..."
-      if ! command -v 'curl' &> /dev/null; then
-        echo "ERROR: impossibile to find command curl. Exiting..."
-        exit 1
-      else
-        DOWNLOAD_CMD="curl ${jq_url} "
-      fi
+  if ! command -v 'wget' &> /dev/null; then
+    echo "ERROR: impossible to find command wget. Trying with curl..."
+    if ! command -v 'curl' &> /dev/null; then
+      echo "ERROR: impossibile to find command curl. Exiting..."
+      exit 1
     else
-      DOWNLOAD_CMD="wget ${jq_url} -O ${jq_installation_path}"
+      DOWNLOAD_CMD="curl ${jq_url} -o ${jq_installation_path}"
     fi
+  else
+    DOWNLOAD_CMD="wget ${jq_url} -O ${jq_installation_path}"
+  fi
 
+  if ! command -v 'jq' &> /dev/null && [ -n "${DOWNLOAD_CMD}" ]; then
     ${DOWNLOAD_CMD}
     chmod +x '/usr/local/bin/jq'
   fi
