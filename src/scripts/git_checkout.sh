5d0b477657eec350da9fbf675f2675b051c0ccca
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-06-14 09:48:59 +0200

    fix permission issue with jq

--- src/scripts/git_checkout.sh
+++ src/scripts/git_checkout.sh
@@ -4,6 +4,7 @@ CheckoutRepo() {
 
   if ! command -v 'jq' &> /dev/null; then
     wget 'https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64' -O '/usr/local/bin/jq'
+    chmod +x '/usr/local/bin/jq'
     jq --version
   fi
 
--- src/scripts/github_pr_read_labels.sh
+++ src/scripts/github_pr_read_labels.sh
@@ -8,6 +8,7 @@ ReadPRLabels() {
 
   if ! command -v 'jq' &> /dev/null; then
     wget 'https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64' -O '/usr/local/bin/jq'
+    chmod +x '/usr/local/bin/jq'
     jq --version
   fi
 
