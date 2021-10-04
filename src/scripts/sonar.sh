9b8cffc68db7fc63f601ec73e36294f944199b71
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-10-04 16:37:36 +0200

    [semver:patch] Run sonar on tagged builds (#122)

--- src/scripts/sonar.sh
+++ src/scripts/sonar.sh
@@ -4,8 +4,8 @@ SONAR_OPTS="${SONAR_OPTS} -Dsonar.host.url=${SONAR_HOST_URL} \
 -Dsonar.password=${SONAR_PASS}"
 
 function run_sonar() {
-  if [ "$CIRCLE_BRANCH" != "master" ] && [ "$CIRCLE_BRANCH" != "main" ] && [ -z "${CI_PULL_REQUEST}" ]; then
-    echo "ERROR: not on a PR nor on master/main, please enable 'Only build pull requests' option in the CircleCI project settings page"
+  if [ "$CIRCLE_BRANCH" != "master" ] && [ "$CIRCLE_BRANCH" != "main" ] && [ -z "${CI_PULL_REQUEST}" ] && [ -z "${CIRCLE_TAG}" ]; then
+    echo "ERROR: not on a PR nor on master/main nor a tagged build, you may want to enable 'Only build pull requests' option in the CircleCI project settings page"
     exit 1
   fi
 
