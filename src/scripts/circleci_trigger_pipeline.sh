36f4507c73c350540bcdc51022f635f7d685cb75
Pietro Paolo Ferrari <pietropaolo.ferrari@eng.it>
2021-11-30 15:56:53 +0100

    [semver:patch] use `Circle-Token` header (#125)

--- src/scripts/circleci_trigger_pipeline.sh
+++ src/scripts/circleci_trigger_pipeline.sh
@@ -12,11 +12,11 @@ if [ -z "${ORG}" ]; then
   exit 1
 fi
 
-if [ -z $BRANCH ] || [ "${BRANCH}" == "" ]; then
+if [ -z "$BRANCH" ] || [ "${BRANCH}" == "" ]; then
   REPO_URL="git@github.com:${ORG}/${PROJECT_NAME}.git"
-  if git ls-remote -h $REPO_URL | grep -q "${CIRCLE_BRANCH}"; then
+  if git ls-remote -h "$REPO_URL" | grep -q "${CIRCLE_BRANCH}"; then
     BRANCH="${CIRCLE_BRANCH}"
-  elif git ls-remote -h $REPO_URL | grep -q "refs/heads/main"; then
+  elif git ls-remote -h "$REPO_URL" | grep -q "refs/heads/main"; then
     BRANCH="main"
   else
     BRANCH="master"
@@ -29,17 +29,30 @@ else
   json_data='{ "parameters": '${PARAMETERS}', "branch": "'${BRANCH}'"}'
 fi
 
+PIPELINE_API_URL="https://circleci.com/api/v2/project/gh/${ORG}/${PROJECT_NAME}/pipeline"
+CIRCLE_RESPONSE_OUTPUT_PATH="circleci_trigger_response_${PROJECT_NAME}.json"
 
 echo "Trigger info: "
 echo ">> Org: ${ORG}"
 echo ">> Project: ${PROJECT_NAME}"
 echo ">> Branch: ${BRANCH}"
 echo ">> Parameters: ${PARAMETERS}"
-echo ">> Url: https://circleci.com/api/v2/project/gh/${ORG}/${PROJECT_NAME}/pipeline?circle-token=${CIRCLECI_TOKEN}"
+echo ">> Url: ${PIPELINE_API_URL}"
 echo -e "\n"
 
 curl \
   --header "Content-Type: application/json" \
+  --header "Circle-Token: ${CIRCLECI_TOKEN}" \
   --data "${json_data}" \
   --request POST \
-  https://circleci.com/api/v2/project/gh/${ORG}/${PROJECT_NAME}/pipeline?circle-token="${CIRCLECI_TOKEN}"
+  "${PIPELINE_API_URL}" -o "${CIRCLE_RESPONSE_OUTPUT_PATH}"
+
+cat "${CIRCLE_RESPONSE_OUTPUT_PATH}"
+PIPELINE_NUMBER=$(jq --raw-output '.number // empty' "${CIRCLE_RESPONSE_OUTPUT_PATH}")
+
+if [ -z "${PIPELINE_NUMBER}" ]; then
+  echo -e "\nSomething went wrong triggering ${PROJECT_NAME} pipeline"
+  exit 1
+else
+  echo -e "\nTriggered ${PROJECT_NAME} pipeline ${PIPELINE_NUMBER}"
+fi
