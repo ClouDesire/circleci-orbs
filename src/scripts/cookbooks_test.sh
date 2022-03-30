0543312a2413331f03a514669304060296bd88b8
Manuel Mazzuola <manuel.mazzuola@eng.it>
2022-03-30 10:25:43 +0200

    [semver:major] Run a converge against only CONCURRENCY	value instances

--- src/scripts/cookbooks_test.sh
+++ src/scripts/cookbooks_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-if [ "$PARAM_KITCHEN_CONCURRENCY" == "false" ]; then
-    export KITCHEN_CONCURRENCY=""
+if [ "$PARAM_KITCHEN_CONCURRENCY" -le "-1" ]; then
+  export KITCHEN_CONCURRENCY="-c"
+elif [ "$PARAM_KITCHEN_CONCURRENCY" -eq "0" ]; then
+  export KITCHEN_CONCURRENCY=""
 else
-    export KITCHEN_CONCURRENCY="-c"
+  export KITCHEN_CONCURRENCY="--concurrency=$PARAM_KITCHEN_CONCURRENCY"
 fi
 
 if [ -z $CONVERGE_NUMBER ]; then
