6661c4194d537e0873552ebbb048cfd49bd95c74
Manuel Mazzuola <1757492+manuelmazzuola@users.noreply.github.com>
2022-03-30 12:14:32 +0200

    [semver:major] Run a converge against only CONCURRENCY value instances (#134)
    ref ClouDesire/support#6308

--- src/scripts/cookbooks_cleanup.sh
+++ src/scripts/cookbooks_cleanup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-if [ "$PARAM_KITCHEN_CONCURRENCY" == "true" ]; then
+if [ "$PARAM_KITCHEN_CONCURRENCY" -le "-1" ]; then
   export KITCHEN_CONCURRENCY="-c"
-else
+elif [ "$PARAM_KITCHEN_CONCURRENCY" -eq "0" ]; then
   export KITCHEN_CONCURRENCY=""
+else
+  export KITCHEN_CONCURRENCY="--concurrency=$PARAM_KITCHEN_CONCURRENCY"
 fi
 source "${BASH_ENV}"
 retry -- chef exec kitchen destroy "($PARAM_KITCHEN_SUITES)" $KITCHEN_CONCURRENCY
